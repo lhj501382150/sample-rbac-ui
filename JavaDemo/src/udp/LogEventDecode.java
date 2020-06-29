27a5070d97b41f5dd9ab8c0f880d7277bfbbe732
lhj501382150 <liuhaojie113@163.com>
2020-06-29 09:58:43 +0800

    udp

--- JavaDemo/src/udp/LogEventDecode.java
+++ JavaDemo/src/udp/LogEventDecode.java
@@ -16,13 +16,13 @@ public class LogEventDecode extends MessageToMessageDecoder<DatagramPacket> {
 		String msg = "";
 		
 		ByteBuf buf = datagramPacket.content();
-		/*int length = buf.readableBytes();
+		int length = buf.readableBytes();
 		int idx = buf.indexOf(0,length , LogEvent.SEPARAOR);
-		 fileName = buf.slice(0, idx).toString(CharsetUtil.UTF_8);
-		 msg = buf.slice(idx+1,length-1).toString(CharsetUtil.UTF_8);
-		*/
+		fileName = buf.slice(0, idx).toString(CharsetUtil.UTF_8);
+		 msg = buf.slice(idx+1,length-idx-1).toString(CharsetUtil.UTF_8);
 		
-		String content = buf.toString(CharsetUtil.UTF_8);
+		
+		/*String content = buf.toString(CharsetUtil.UTF_8);
 		String split = String.valueOf((char)LogEvent.SEPARAOR);
 		if(content.contains(split)){
 			String[] para = content.split(split);
@@ -30,7 +30,7 @@ public class LogEventDecode extends MessageToMessageDecoder<DatagramPacket> {
 				fileName = para[0];
 				msg = para[1];
 			}
-		}
+		}*/
 		LogEvent logEvent = new LogEvent(datagramPacket.sender(), fileName, msg, System.currentTimeMillis());
 		out.add(logEvent);
 	}
